6de814febe9ecbe2ac52d09a58933e466faf08ea
sehabkhanzehad <sehabkhanzehad@yahoo.com>
2024-09-29 18:35:45 +0600

    sql update for live

--- sql_database/car_rental.sql
+++ sql_database/car_rental.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Sep 28, 2024 at 01:37 PM
+-- Generation Time: Sep 29, 2024 at 12:35 PM
 -- Server version: 8.0.30
 -- PHP Version: 8.1.10
 
